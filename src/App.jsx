40ba0450b9ce572de200d6f63322a7b3825b4e32
delta <sumitprasad102@gmail.com>
2025-05-12 20:47:22 +0530

    defaut margin given

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 import Home from "../src/components/Home";
 import AboutUs from '../src/components/AboutUs';
 import Product from '../src/components/Product';
